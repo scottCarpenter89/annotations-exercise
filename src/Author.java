55297b441b0fc3b63e66e085e26302f1f781c476
Scott Carpenter <carpenter.scott22@gmail.com>
2022-03-14 16:52:53 -0500

    better_way branch fixed

--- src/Author.java
+++ src/Author.java
@@ -1,6 +1,7 @@
 /**
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,7 +30,7 @@ public class Author extends Person {
     }
 
     @Override
-    public String sortName() {
+    public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
